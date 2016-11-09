87e28c3c65ada697d0798d115e2c9653d7932870
dwicao <lutfiandwica@gmail.com>
2016-11-10 06:11:24 +0700

    fix http to https request

--- omdb-movie-search/js/app.js
+++ omdb-movie-search/js/app.js
@@ -1,17 +1,15 @@
 $(function() {
 
-	$('#search').keypress(function(evt) {
-		if (evt.which === 13) {
-			movieSearch();
-			evt.preventDefault();
+	$('#search').keypress(function(e) {
+		if (e.which === 13) {
+			//movieSearch();
+			alert($('#search').val());
 		}
 	});
 
-	$('#submit').click(function(evt) {
-		movieSearch();
-		evt.preventDefault();
+	$('#submit').click(function() {
+		alert($('#search').val());
 	});
-
 
 	function movieSearch() {
 		var searchInput = $('#search').val();
@@ -21,59 +19,18 @@ $(function() {
 			type: "GET",
 			dataType: "json",
 			success: function(data, status, jqXHR) {
-				var dataArr = data.Search;
-				var movieHTML = '';
-				if (data.Response === 'False') {
-					movieHTML += '<li class="no-movies">';
-					movieHTML += '<i class="material-icons icon-help">';
-					movieHTML += 'help_outline';
-					movieHTML += '</i>';
-					movieHTML += 'No movies found that match: ';
-					movieHTML += searchInput + '.';
-					movieHTML += '</li>';
-					populateIdWithHTML('movies', movieHTML);
-				} 
-				else {
-					for (var i = 0; i < dataArr.length; i++) {
-						movieHTML += '<li>';
-						movieHTML += '<a target="_blank" href="http://www.imdb.com/title/' + dataArr[i].imdbID + '/">';
-						movieHTML += '<div class="poster-wrap">';
-						if (dataArr[i].Poster === 'N/A') {
-							movieHTML += '<i class="material-icons poster-placeholder">crop_original</i>';
-						} else {
-							movieHTML += '<img class="movie-poster" src="';
-							movieHTML +=  dataArr[i].Poster;
-							movieHTML += '">';
-						}
-						movieHTML += '</div>';
-						movieHTML += '<span class="movie-title">';
-						movieHTML += dataArr[i].Title;
-						movieHTML += '</span>';
-						movieHTML += '<span class="movie-year">';
-						movieHTML += dataArr[i].Year;
-						movieHTML += '</span>';
-						movieHTML += '</a>';
-						movieHTML += '</li>';
-						populateIdWithHTML('movies', movieHTML);
-					}
-				}
-				$('#search').val('');		
+				console.log(data);
 			}
 		})
 		.done(function() {
-		//	console.log("success");
+			console.log("success");
 		})
 		.fail(function() {
-		//	console.log("error");
+			console.log("error");
 		})
 		.always(function() {
-		//	console.log("complete");
+			console.log("complete");
 		});
-	}
-
-	function populateIdWithHTML(id, text) {
-		var element = document.getElementById(id);
-		element.innerHTML = text;
 	}
 
 });
